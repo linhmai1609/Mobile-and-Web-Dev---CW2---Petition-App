b1a5ea764a2fd8333c09397216900ccf66845e39
Bao-Linh Le Mai <mailebaolinh@gmail.com>
2024-12-24 09:44:55 +0000

    Add new Dim and Fact data models; Update Alembic migration

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,4 @@
+import datetime 
 import uuid
 
 from pydantic import EmailStr
@@ -44,6 +45,8 @@ class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     hashed_password: str
     items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
+    # created_at: float = Field(default=datetime.datetime.now().replace(tzinfo=datetime.timezone.utc).timestamp())
+    # updated_at: float = Field(default=datetime.datetime.now().replace(tzinfo=datetime.timezone.utc).timestamp())
 
 
 # Properties to return via API, id is always required
@@ -92,7 +95,6 @@ class ItemsPublic(SQLModel):
     data: list[ItemPublic]
     count: int
 
-
 # Generic message
 class Message(SQLModel):
     message: str
@@ -112,3 +114,94 @@ class TokenPayload(SQLModel):
 class NewPassword(SQLModel):
     token: str
     new_password: str = Field(min_length=8, max_length=40)
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Dim_Petition
+
+# Database model, database table inferred from class name
+class Dim_Petition(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    status: str = Field(min_length=1, max_length=255)
+    petition_title: str | None = Field(default=None)
+    petition_text: str | None = Field(default=None)
+    petitioner: EmailStr = Field(max_length=255)
+    response: str | None = Field(default=None)
+    # created_at: float = Field(default=datetime.datetime.now().replace(tzinfo=datetime.timezone.utc).timestamp())
+    # updated_at: float = Field(default=datetime.datetime.now().replace(tzinfo=datetime.timezone.utc).timestamp())
+    # facts: list["Facts_Petition"] = Relationship(back_populates="facts_petition")
+
+# Properties to return via API, id is always required
+class Dim_PetitionPublic(SQLModel):
+    id: uuid.UUID
+    status: str = Field(min_length=1, max_length=255)
+    petition_title: str | None = Field(default=None)
+    petition_text: str | None = Field(default=None)
+    petitioner: EmailStr = Field(max_length=255)
+    response: str | None = Field(default=None)
+    signatures: int
+
+class Dim_PetitionsPublic(SQLModel):
+    petitions: list[Dim_PetitionPublic]
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Dim_BioID
+# Shared properties
+class Dim_BioIDBase(SQLModel):
+    enabled: bool = Field(default=False)
+
+# Database model, database table inferred from class name
+class Dim_BioID(Dim_BioIDBase, table=True):
+    bioid: str| None = Field(
+        min_length=10, 
+        max_length=10, 
+        primary_key=True
+    )
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Dim_User
+# Shared properties
+class Dim_UserBase(SQLModel):
+    email: EmailStr = Field(unique=True, index=True, max_length=255)
+    full_name: str = Field(max_length=255)
+    dob: datetime.date
+    is_active: bool = True
+    is_superuser: bool = False
+
+# Database model, database table inferred from class name
+class Dim_User(Dim_UserBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    hashed_password: str
+    bioid: str | None = Field(
+        unique=True
+        , index=True
+        , max_length=10
+        , foreign_key="dim_bioid.bioid"
+    )
+    created_at: float = Field(default=datetime.datetime.now().replace(tzinfo=datetime.timezone.utc).timestamp())
+    updated_at: float = Field(default=datetime.datetime.now().replace(tzinfo=datetime.timezone.utc).timestamp())
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Facts_Petition
+# Shared properties
+class Facts_PetitionBase(SQLModel):
+    action: str = Field(min_length=1, max_length=255, index=True)
+    message: str | None = Field(default=None)
+
+# Properties to receive via API on creation
+class Facts_PetitionCreate(Facts_PetitionBase):
+    action: str = Field(min_length=1, max_length=255)
+    message: str | None = Field(default=None)
+    
+# Database model, database table inferred from class name
+class Facts_Petition(Facts_PetitionBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    petition_id: uuid.UUID = Field(
+        foreign_key="dim_petition.id", nullable=True, index=True
+    )
+    petition_affector_id: uuid.UUID = Field(
+        foreign_key="dim_user.id", nullable=True, index=True
+    )
+    created_at: float = Field(default=datetime.datetime.now().replace(tzinfo=datetime.timezone.utc).timestamp())
